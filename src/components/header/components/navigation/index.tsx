14fcc01634a24d2d91e00286c8553ab3f652e511
bruno-candia <bruno852812@gmail.com>
2024-05-29 18:11:34 -0300

    fix(navigation): move the navigation component into the header component folder

--- src/components/header/components/navigation/index.tsx
+++ src/components/header/components/navigation/index.tsx
@@ -1,45 +1,45 @@
 import Link from 'next/link'
-import styles from './styles.module.css'
+import styles from '../../styles.module.css'
 
 export default function Navigation() {
   return (
-    <nav aria-label="principal menu">
-      <ul className={styles.navigationList}>
-        <li className={styles.navigationItem}>
+    <nav aria-label="principal menu" className={styles.is_desktop}>
+      <ul className={styles.navigation_list}>
+        <li className={styles.navigation_item}>
           <Link
-            className={styles.navigationLink}
+            className={styles.navigation_link}
             href="#about-me"
             id="about-me"
           >
             Sobre Mim
           </Link>
         </li>
-        <li className={styles.navigationItem}>
+        <li className={styles.navigation_item}>
           <Link
-            className={styles.navigationLink}
+            className={styles.navigation_link}
             href="#my-content"
             id="my-content"
           >
             Meu Conteúdo
           </Link>
         </li>
-        <li className={styles.navigationItem}>
-          <Link className={styles.navigationLink} href="#my-lab" id="my-lab">
+        <li className={styles.navigation_item}>
+          <Link className={styles.navigation_link} href="#my-lab" id="my-lab">
             Meu Lab
           </Link>
         </li>
-        <li className={styles.navigationItem}>
+        <li className={styles.navigation_item}>
           <Link
-            className={styles.navigationLink}
+            className={styles.navigation_link}
             href="#experiences"
             id="experiences"
           >
             Experiências
           </Link>
         </li>
-        <li className={styles.navigationItem}>
+        <li className={styles.navigation_item}>
           <Link
-            className={styles.navigationLink}
+            className={styles.navigation_link}
             href="#jobs-and-history"
             id="jobs-and-history"
           >
