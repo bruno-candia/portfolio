a51ba92277e7e5e5224c3224b591d49decf1caf8
bruno-candia <bruno852812@gmail.com>
2026-01-13 22:22:28 -0300

    chore(audit): standardize domain to brunocandia.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://brunocandia.com.br/sitemap.xml',
+    sitemap: 'https://brunocandia.com/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://brunocandia.com.br';
+  const baseUrl = 'https://brunocandia.com';
 
   return [
     {
